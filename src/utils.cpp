e0de81bba2e1ecc60ba43af6c571c8b63eb1c7af
Eugene316 <92687071+Eugene316@users.noreply.github.com>
2023-01-21 00:27:22 +0700

    feat: utils

---
+++ src/utils.cpp
@@ -0,0 +1,88 @@
+#include "utils.h"
+
+clock_t checkpoint;
+
+/* records current clock */
+void start_timer() {
+    checkpoint = clock();
+}
+
+/* displays time elapsed since last checkpoint */
+void stop_timer() {
+    cout << (double)(clock() - checkpoint) / CLOCKS_PER_SEC;
+}
+
+/* get card value (1-10) of input string */
+int card_value(string input) {
+    if (input == "A") {
+        return 1;
+    } else if (input == "10" || input == "J" || input == "Q" || input == "K") {
+        return 10;
+    } else if (input == "2") {
+        return 2;
+    } else if (input == "3") {
+        return 3;
+    } else if (input == "4") {
+        return 4;
+    } else if (input == "5") {
+        return 5;
+    } else if (input == "6") {
+        return 6;
+    } else if (input == "7") {
+        return 7;
+    } else if (input == "8") {
+        return 8;
+    } else if (input == "9") {
+        return 9;
+    } else {
+        return -1;
+    }
+}
+
+/* input 4 cards by user */
+void input_cards(int * cards) {
+    cout << "Input 4 cards! e.g. A 2 10 Q" << endl;
+    string inputs[4];
+    do {
+        cin >> inputs[0] >> inputs[1] >> inputs[2] >> inputs[3];
+
+        for (int i = 0; i < 4; i++) {
+            cards[i] = card_value(inputs[i]);
+        }
+        
+        if (cards[0] == -1 || cards[1] == -1 || cards[2] == -1 || cards[3] == -1) {
+            cout << "Invalid input!" << endl;
+        }
+    } while (cards[0] == -1 || cards[1] == -1 || cards[2] == -1 || cards[3] == -1);
+}
+
+/* randomize 4 cards */
+void random_cards(int * cards) {
+    srand(time(NULL));
+    int random;
+    for (int i = 0; i < 4; i++) {
+        random = rand() % 13 + 1;
+        switch (random) {
+            case 1: {
+                cards[i] = 1;
+                cout << "A ";
+                break;
+            } case 11: {
+                cards[i] = 10;
+                cout << "J ";
+                break;
+            } case 12: {
+                cards[i] = 10;
+                cout << "Q ";
+                break;
+            } case 13: {
+                cards[i] = 10;
+                cout << "K ";
+                break;
+            } default:
+                cards[i] = random;
+                cout << to_string(random) + ' ';
+                break;
+        }
+    }
+}
---
+++ src/utils.h
@@ -0,0 +1,26 @@
+#ifndef UTILS
+#define UTILS
+
+#include <random>
+#include <time.h>
+#include <iostream>
+using namespace std;
+
+extern clock_t checkpoint;
+
+/* records current clock */
+void start_timer();
+
+/* displays time elapsed since last checkpoint */
+void stop_timer();
+
+/* get card value (1-10) of input string */
+int card_value(string input);
+
+/* input 4 cards by user */
+void input_cards(int * cards);
+
+/* randomize 4 cards */
+void random_cards(int * cards);
+
+#endif
