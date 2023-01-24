37499592e106deb73058d611f5c5b77809f9fc46
Eugene316 <92687071+Eugene316@users.noreply.github.com>
2023-01-24 15:36:48 +0700

    feat: permute cards

--- src/main.cpp
+++ src/main.cpp
@@ -30,25 +30,62 @@ int main() {
 
     /* chore: permute cards*/
 
-    v_expr perms;   /* perm: abcd */
-    for (int i = 0; i < 4; i++) {
-        perms.push_back(create_expr(card_inputs[i]));
-    }
+    v_expr perms = {};   /* perm: abcd */
 
-
+    string curr_perm;   /* current permutation */
+    vector<string> processed = {};  /* processed perms */
+    bool is_new_perm;
+        
     /* solve & view solutions */
     cout << "\nSolusi:" << endl;
 
     start_timer();
-    solve(perms);
-    cout << "Waktu pencarian: ";
+    // solve(perms);
+    for (int i = 0; i < 4; i++) {       /* permutation loop */
+        for (int j = 0; j < 4; j++) {
+            for (int k = 0; k < 4; k++) {
+                for (int l = 0; l < 4; l++) {
+                    if (i != j && i != k && i != l && j != k && j != l && k != l) {     /* process if i,j,k,l indices are unique */
+                        curr_perm = to_string(card_inputs[i]) + to_string(card_inputs[j]) + to_string(card_inputs[k]) + to_string(card_inputs[l]);
+
+                        is_new_perm = true;         /* check if permutation is previously processed */
+                        for (int a = 0; a < processed.size(); a++) {
+                            if (curr_perm == processed[a]) {
+                                is_new_perm = false;
+                                break;
+                            }
+                        }
+
+                        if (is_new_perm) {      /* process if permutation is new */
+                            processed.push_back(curr_perm);
+                            
+                            perms.clear();      /* create permutation v_expr */
+                            perms.push_back(create_expr(card_inputs[i]));
+                            perms.push_back(create_expr(card_inputs[j]));
+                            perms.push_back(create_expr(card_inputs[k]));
+                            perms.push_back(create_expr(card_inputs[l]));
+
+                            solve(perms);
+                        }
+                    }
+                }
+            }
+        }   
+    }
     stop_timer();
-    cout << endl;
 
-    display_solut();
-    cout << "Banyak solusi: " << solutions.size() << endl;
+    int total_solut = solutions.size();
 
-    /* chore: save to file */
+    if (total_solut > 0) {
+        cout << "Banyak solusi: " << total_solut << endl;
+        display_solut();
+        /* chore: save to file */
+    } else {
+        cout << "Tidak ada solusi" << endl;
+    }
+
+    cout << "Waktu pencarian: " << recorded_time << " detik" << endl;
+    
 
     return 0;
 }
